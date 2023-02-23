c58a9c4dcee8b3c507b058a7ef4cc92b4972b724
INlOEtdF <krasheg@gmail.com>
2023-02-23 11:42:20 +0200

    updated script

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY memory_usage_script.py .
-CMD [ "python", "memory_usage_script.py" ]
+CMD [ "python", "-u","memory_usage_script.py" ]
--- memory_usage_script.py
+++ memory_usage_script.py
@@ -1,24 +1,38 @@
 """ Works only on linux systems"""
 import resource
 import requests
+from requests.exceptions import ConnectionError
+import time
 
 
 def memory_manager(memory_limit_in_bytes, url=None):
-    resource.setrlimit(resource.RLIMIT_AS, (memory_limit_in_bytes, memory_limit_in_bytes))
-
     if resource.getrusage(resource.RUSAGE_SELF).ru_maxrss > memory_limit_in_bytes:
-        data = {'message': 'Memory usage exceeded the limit!'}
+        data = {'key': 'alarm',
+                'value': f'Memory usage exceeded the limit!({resource.getrusage(resource.RUSAGE_SELF).ru_maxrss} KB)'}
         if not url:
             print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             return
-        response = requests.post(url, data=data)
-        return response.text
-    mem_init = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        try:
+            response = requests.post(url, json=data)
+            print('Alarm message sent!')
+            return response.status_code
+        except ConnectionError:
+            print('failed to send alarm message')
+            return 'Connection refused'
+
     mem_current = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    print(f"Initial memory usage: {mem_init} KB")
     print(f"Current memory usage: {mem_current} KB")
-    
+    return mem_current
+
+
+def run_script(memory_limit, url):
+    while True:
+        print('starting script')
+        memory_manager(memory_limit, url)
+        time.sleep(10)
+
 
 if __name__ == "__main__":
-    memory_limit = 100 * 1024 * 1024  # 100 Mb
-    memory_manager(memory_limit)
+    memory_limit = 20000  # 
+    url = 'http://localhost:8080/api/create'
+    run_script(memory_limit, url)
